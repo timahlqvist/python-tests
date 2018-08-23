b65e940d5ad2f2e421c635a83f8d192139878b6f
Tim Ahlqvist <timahlqvist.arbete@gmail.com>
2018-08-23 20:24:48 +0200

    IF

---
+++ IF/if.py
@@ -0,0 +1,8 @@
+age = 1
+
+if age >= 18:
+    print("Nu får du köra bil! :)")
+elif age >= 15 and age < 18:
+    print("Nu får du äntligen köra moppe")
+else:
+    print("Tyvärr, du får vänta några år till innan du får köra ett fordon")
